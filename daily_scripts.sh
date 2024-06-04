de536ef8d8241462c742e505034a2630e5df6022
André Nordbø <andre.nordbo@gmail.com>
2024-06-04 19:41:19 +0200

    Update daily_scripts.sh

--- daily_scripts.sh
+++ daily_scripts.sh
@@ -8,11 +8,11 @@ source /var/web/py311/bin/activate
 cd /var/web/VulnTracker
 
 # Run the scripts
-python3 manage.py fetch_cisa_known_exploited
-python3 manage.py fetch_cves -p past_day
-python3 manage.py fetch_haveibeenpwned
-python3 manage.py fetch_nessus
-python3 manage.py fetch_shodan
-python3 manage.py fetch_software
-python3 manage.py fetch_vulnerabilities
-python3 manage.py fetch_machine_vulns.py
+python manage.py fetch_cisa_known_exploited
+python manage.py fetch_cves -p past_day
+python manage.py fetch_haveibeenpwned
+python manage.py fetch_nessus
+python manage.py fetch_shodan
+python manage.py fetch_software
+python manage.py fetch_vulnerabilities
+python manage.py fetch_machine_vulns.py
