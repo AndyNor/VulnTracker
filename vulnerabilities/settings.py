83c87a4e823b90de5cca0c97338b816dc69d9285
André Nordbø <andre.nordbo@gmail.com>
2024-06-04 19:23:25 +0200

    Fikse static dirs

--- vulnerabilities/settings.py
+++ vulnerabilities/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = (f'{BASE_DIR}/static',)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATICFILES_DIRS = (f'{BASE_DIR}/static',)
 
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = BASE_DIR / 'media'
