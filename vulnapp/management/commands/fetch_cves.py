9edbbd3aef4ff952ca32626ec26a2b8112b8c2bb
André Nordbø <andre.nordbo@gmail.com>
2024-11-26 09:47:29 +0100

    6 retries for fetch_cves

--- vulnapp/management/commands/fetch_cves.py
+++ vulnapp/management/commands/fetch_cves.py
@@ -2,14 +2,14 @@ from django.core.management.base import BaseCommand, CommandError
 from vulnapp.models import *
 from datetime import datetime, timedelta
 from django.utils import timezone
-import re, os, json, pytz, requests
+import re, os, json, pytz, requests, time
 
 class Command(BaseCommand):
 	help = 'Fetch and store CVE data from NVD'
 
-	def add_arguments(self, parser):
-		# Define a command line argument for the command
-		parser.add_argument('-p', '--period', type=str, help='Period to fetch CVEs for (past_day, past_week, past_month)')
+	#def add_arguments(self, parser):
+	#	# Define a command line argument for the command
+	#	parser.add_argument('-p', '--period', type=str, help='Period to fetch CVEs for (past_day, past_week, past_month)')
 
 	def handle(self, *args, **options):
 		self.base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0/"
@@ -21,19 +21,19 @@ class Command(BaseCommand):
 		scan_type = "NIST CVEs"
 		scan_status = ScanStatus.objects.create(scan_type=scan_type, status='in_progress', details='{}')
 
-		period = options.get('period', 'past_day')
+		#period = options.get('period', 'past_day')
 
 		# fetch and store vulnerabilities to database
 		try:
-			if period == 'past_day':
-				cve_data = self.fetch_cves_past_day()
-			elif period == 'past_week':
-				cve_data = self.fetch_cves_past_week()
-			elif period == 'past_month':
-				cve_data = self.fetch_cves_past_month()
-			else:
-				self.stdout.write(self.style.ERROR('No period specified. Using past_week'))
-				cve_data = self.fetch_cves_past_week()
+		#	if period == 'past_day':
+			cve_data = self.fetch_cves_past_3day()
+		#	elif period == 'past_week':
+		#		cve_data = self.fetch_cves_past_week()
+		#	elif period == 'past_month':
+		#		cve_data = self.fetch_cves_past_month()
+		#	else:
+		#		self.stdout.write(self.style.ERROR('No period specified. Using past_week'))
+		#		cve_data = self.fetch_cves_past_week()
 
 			if cve_data:
 				self.save_cve_data(cve_data, scan_status)  # Corrected to pass scan_status
@@ -57,7 +57,7 @@ class Command(BaseCommand):
 			try:
 				keyword_list = [keyword.word.lower() for keyword in Keyword.objects.all()]
 				programvarelev_list = [keyword.word.lower() for keyword in ProgramvareLeverandorer.objects.all()]
-				start_date = timezone.now() - timedelta(days=14)
+				start_date = timezone.now() - timedelta(days=7)
 				recent_cves = CVE.objects.filter(published_date__gte=start_date)
 
 				def findWholeWord(word):
@@ -94,35 +94,42 @@ class Command(BaseCommand):
 
 
 
-	def fetch_cves_past_day(self):
+	def fetch_cves_past_3day(self):
 		today = datetime.utcnow()
-		yesterday = today - timedelta(days=1)
+		yesterday = today - timedelta(days=3)
 		return self.fetch_cves(yesterday.strftime('%Y-%m-%d'), today.strftime('%Y-%m-%d'))
 
 	def fetch_cves(self, date_start, date_end):
+
+		retries_left = 6
+
 		session = requests.Session()
 		params = {
 			'pubStartDate': f'{date_start}T00:00:00.000',
 			'pubEndDate': f'{date_end}T23:59:59.000'
 		}
 
-		print(f"Connecting to {self.base_url} with {self.headers}")
+		print(f"Connecting to {self.base_url}")
 		print(f"Using parameters {params}")
-		try:
-			response = session.get(self.base_url, headers=self.headers, params=params, timeout=120, stream=True)
-			print(f"Getting {response.status_code} from service..")
-			if response.status_code == 200:
-				data = []
-				for chunk in response.iter_content(chunk_size=8192):
-					data.append(chunk)
-				full_data = b''.join(data).decode('utf-8')
-				return full_data  # Return raw JSON string for now
-			else:
-				self.stdout.write(f"Error: Received status code {response.status_code}")
-				return None
-		except requests.RequestException as e:
-			self.stdout.write(f"Request failed: {e}")
-			return None
+		while retries_left >= 0:
+			try:
+				response = session.get(self.base_url, headers=self.headers, params=params, timeout=(5, 30), stream=True)
+				print(f"Getting {response.status_code} from service..")
+				if response.status_code == 200:
+					data = []
+					for chunk in response.iter_content(chunk_size=8192):
+						data.append(chunk)
+					full_data = b''.join(data).decode('utf-8')
+					return full_data  # Return raw JSON string for now
+				else:
+					self.stdout.write(f"Error: Received status code {response.status_code}")
+					#return None
+			except requests.RequestException as e:
+				self.stdout.write(f"Request failed: {e}")
+				#return None
+			retries_left -= 1 # reduce retry counter
+			print(f"Waiting 5 minutes for next attempt. {retries_left} retries left.")
+			time.sleep(300)
 
 	def fetch_cves_past_week(self):
 		today = datetime.utcnow()
